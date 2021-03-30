f604b69d35381fa1c23a3d07b628f2ecc5bdbab3
Musa Bojang <musabojang413@gmail.com>
2021-03-30 12:00:08 +0800

    initial commits

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,22 @@
+import React, {useContext} from "react";
+import "./Footer.css";
+import {Fade} from "react-reveal";
+import emoji from "react-easy-emoji";
+import StyleContext from "../../contexts/StyleContext";
+
+export default function Footer() {
+  const {isDark} = useContext(StyleContext);
+  return (
+    <Fade bottom duration={1000} distance="5px">
+      <div className="footer-div">
+        {/* <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+          {emoji("Made by Saad Pasta | Modified by David Rakosi @CleverProgrammer")}
+        </p> */}
+        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+          All Rights Reserved &#169; 2021 
+          
+        </p>
+      </div>
+    </Fade>
+  );
+}
---
+++ src/containers/greeting/Greeting.js
@@ -0,0 +1,65 @@
+import React, {useContext} from "react";
+import {Fade} from "react-reveal";
+import emoji from "react-easy-emoji";
+import "./Greeting.css";
+import landingPerson from "../../assets/lottie/landingPerson";
+import DisplayLottie from "../../components/displayLottie/DisplayLottie";
+import SocialMedia from "../../components/socialMedia/SocialMedia";
+import Button from "../../components/button/Button";
+
+import {illustration, greeting} from "../../portfolio";
+import StyleContext from "../../contexts/StyleContext";
+
+export default function Greeting() {
+  const {isDark} = useContext(StyleContext);
+  if (!greeting.displayGreeting) {
+    return null;
+  }
+  return (
+    <Fade bottom duration={1000} distance="40px">
+      <div className="greet-main" id="greeting">
+        <div className="greeting-main">
+          <div className="greeting-text-div">
+            <div>
+              <h1
+                className={isDark ? "dark-mode greeting-text" : "greeting-text"}
+              >
+                {" "}
+                {greeting.title}{" "}
+                <span className="wave-emoji">{emoji("👋")}</span>
+              </h1>
+              <p
+                className={
+                  isDark
+                    ? "dark-mode greeting-text-p"
+                    : "greeting-text-p subTitle"
+                }
+              >
+                {greeting.subTitle}
+              </p>
+              <SocialMedia />
+              <div className="button-greeting-div">
+                <Button text="WhatsApp" href="https://api.whatsapp.com/send?phone=601137068713&text=Hi%20musa,%20I%20have%20this%20project%20for%20you" />
+                <Button
+                  text="Messenger"
+                  newTab={true}
+                  href="https://msng.link/o/?https://www.facebook.com/musabojanggeneralmassup/=fm"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="greeting-image-div">
+            {illustration.animated ? (
+              <DisplayLottie animationData={landingPerson} />
+            ) : (
+              <img
+                alt="man sitting on table"
+                src={require("../../assets/images/manOnTable.svg")}
+              ></img>
+            )}
+          </div>
+        </div>
+      </div>
+    </Fade>
+  );
+}
